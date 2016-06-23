2883f1b01daf98d52b78f7de05ae4f9a15532d3c
HiepBP <phuhiep11395@gmail.com>
2016-06-23 20:47:56 +0700

    Initializa new project

---
+++ Vimas/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vimas.MvcApplication" Language="C#" %>
---
+++ Vimas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vimas.Startup))]
+namespace Vimas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

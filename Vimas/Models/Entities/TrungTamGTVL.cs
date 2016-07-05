2addb5a091e2d685da1dff3a3a4d0a84fc018893
HiepBP <phuhiep11395@outlook.com>
2016-07-05 20:50:04 +0700

    Update db and entities

--- Vimas/Models/Entities/TrungTamGTVL.cs
+++ Vimas/Models/Entities/TrungTamGTVL.cs
@@ -30,6 +30,7 @@ namespace Vimas.Models.Entities
         public Nullable<System.DateTime> NgayKyHopDong { get; set; }
         public Nullable<System.DateTime> NgayHetHan { get; set; }
         public string NguoiDaiDien { get; set; }
+        public string ChucDanh { get; set; }
         public Nullable<bool> Active { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]

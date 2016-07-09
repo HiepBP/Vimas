e4e43803244accb13a7748dfefe44089b627ab0e
Lucas1805 <khaithong08@gmail.com>
2016-07-09 09:14:51 +0700

    Edit và Delete cho NghiepDoan. Sửa bug delete không confirm cho TrungTamGTVL

--- Vimas/Areas/HeThong/Controllers/NghiepDoanController.cs
+++ Vimas/Areas/HeThong/Controllers/NghiepDoanController.cs
@@ -75,5 +75,106 @@ namespace Vimas.Areas.HeThong.Controllers
         }
         #endregion
 
+        #region Create
+        [Authorize(Roles = "Admin, PhongNguon")]
+        public ActionResult Create()
+        {
+            var model = new NghiepDoanViewModel();
+            return this.View(model);
+        }
+        [HttpPost]
+        [Authorize(Roles = "Admin, PhongXKLD")]
+        public async Task<ActionResult> Create(NghiepDoanViewModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            var service = this.Service<INghiepDoanService>();
+            model.Active = true;
+            try
+            {
+                await service.CreateAsync(model.ToEntity());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return this.RedirectToAction("Index");
+        }
+        #endregion
+
+        #region Edit
+        [Authorize(Roles = "Admin, PhongNguon")]
+        public ActionResult Edit(int id)
+        {
+            var service = this.Service<INghiepDoanService>();
+
+            var entity = service.Get(id);
+
+            if (entity == null)
+            {
+                return HttpNotFound();
+            }
+
+            var model = Mapper.Map<NghiepDoanViewModel>(entity);
+
+            return this.View(model);
+        }
+
+        [Authorize(Roles = "Admin, PhongNguon")]
+        [HttpPost]
+        public async Task<JsonResult> Edit(NghiepDoanViewModel model)
+        {
+            var service = this.Service<INghiepDoanService>();
+
+            if (!this.ModelState.IsValid)
+            {
+                return Json(new { success = false, message = "Đã có lỗi xảy ra, vui lòng kiểm tra lại thông tin." });
+            }
+
+            try
+            {
+                await service.UpdateAsync(model.ToEntity());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return Json(new { success = false, message = "Đã có lỗi xảy ra, vui lòng kiểm thử lại." });
+            }
+
+            return Json(new { success = true, message = "Chỉnh sửa thông tin thành công." });
+        }
+        #endregion
+
+        #region Delete
+        [HttpPost]
+        [Authorize(Roles = "Admin, PhongXKLD")]
+        public async Task<JsonResult> Delete(int id)
+        {
+            var nghiepDoanService = this.Service<INghiepDoanService>();
+
+            var entity = nghiepDoanService.Get(id);
+
+            if (entity == null)
+            {
+                return Json(new { success = false, message = "Không tồn tại trung tâm này, xin vui lòng thử lại." });
+            }
+            else
+            {
+                try
+                {
+                    entity.Active = false;
+                    await nghiepDoanService.UpdateAsync(entity);
+                }
+                catch (Exception e)
+                {
+                    return Json(new { success = false, message = "Xóa trung tâm thất bại, xin vui lòng thử lại." });
+                };
+            }
+            return Json(new { success = true, message = "Xóa trung tâm thành công." });
+        }
+        #endregion
     }
 }
